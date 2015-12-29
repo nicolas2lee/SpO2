c286605dfd89514c3b218538e8ee702457d1b15d
nicolas2lee <nicolas2lee@gmail.com>
2015-12-29 21:57:32 +0100

    add MPAndroidChart to draw static graph

---
+++ HealthDetector/app/src/main/java/ensta/bretagne/healthdetector/GUIActivity.java
@@ -0,0 +1,32 @@
+package ensta.bretagne.healthdetector;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class GUIActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gui);
+		findViewById(R.id.show_chart_button).setOnClickListener(new View.OnClickListener(){
+
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(getBaseContext(), StaticChartsActivity.class));
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_gui, menu);
+		return true;
+	}
+
+}
---
+++ HealthDetector/app/src/main/java/ensta/bretagne/healthdetector/RealTimeChartActivity.java
@@ -0,0 +1,14 @@
+package ensta.bretagne.healthdetector;
+//https://www.youtube.com/watch?v=a20EchSQgpw
+import android.os.Bundle;
+import android.app.Activity;
+
+public class RealTimeChartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real_time_chart);
+    }
+
+}
